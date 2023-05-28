41eea042ba65369d632bdf4178b9773d11eb963e
unknown <bryan.brasil.bl@gmail.com>
2023-05-27 23:30:51 -0300

    Metodo res.send criado

--- src/controller/usercontroller.js
+++ src/controller/usercontroller.js
@@ -1,27 +1,24 @@
 const users = require('../mocks/users')
+
 module.exports = {
-  listUsers(req, res){
-    const {order} = req.query;
+  listUsers (req, res){
+    const {order} = req.query
     const sortedUsers = users.sort((a, b)=>{
       if(order === 'desc'){
-        return a.id < b.id ? 1 : -1
+        return a.id < b.id ? 1 : -1;
       }
-      return a.id > b.id ? 1 : -1
-    });
-
-    res.writeHead(200, {'content-type': 'application/json'})
-    res.end(JSON.stringify(sortedUsers))
+      return a.id > b.id ? 1 : -1;
+    })
+    res.send(200, sortedUsers);
   },
   getUserById(req, res){
-    const { id } = req.params
+    const {id} = req.params
 
-    const user = users.find((user) => user.id === Number(id))
+    const user = users.find((user)=> user.id === Number(id))
 
     if(!user){
-      res.writeHead(400, {'content-type': 'application/json'})
-    res.end(JSON.stringify({erro: 'user not found'}))
+      return res.send(404,{error:`Not Found`})
     }
-    res.writeHead(200, {'content-type': 'application/json'})
-    res.end(JSON.stringify({user}))
+    res.send(200,user)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 const http = require('http')
-const {URL} = require('url')
+const { URL } = require('url')
 const routes = require('./routes')
 
-
-const server = http.createServer((req, res)=> {
+const server = http.createServer((req, res) => {
   const parsedURL = new URL(`http://localhost:3000${req.url}`)
-  console.log(`Request method ${req.method} | Endpoint ${parsedURL.pathname}`)
-
-  let {pathname} = parsedURL
+  console.log(`Request Method ${req.method} | Endpoint ${parsedURL.pathname}`);
+  let { pathname } = parsedURL
   let id = null
   const splitEndpoint = pathname.split('/').filter(Boolean)
-
-  if(splitEndpoint.length > 1){
+  if (splitEndpoint.length > 1) {
     pathname = `/${splitEndpoint[0]}/:id`
     id = splitEndpoint[1];
   }
   console.log(splitEndpoint)
-  const route = routes.find((routeObj)=>(
-    routeObj.endpoint === pathname && routeObj.method === req.method
+
+  const route = routes.find((routeOBJ) => (
+    routeOBJ.endpoint === pathname && routeOBJ.method === req.method
   ))
-  if(route){
+  if (route) {
     req.query = Object.fromEntries(parsedURL.searchParams)
-    req.params = {id};
+    req.params = { id }
+
+      res.send = (statuscode, body)=>{
+      res.writeHead(statuscode, { 'content-type': 'application/json' })
+      res.end(JSON.stringify(body))
+    }
     route.handler(req, res)
-  }else{
-    res.writeHead(404, {'content-type': 'text/html'})
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' })
     res.end(`Cannot ${req.method} ${pathname}`)
   }
+
 })
-server.listen(3000, console.log('Server rodando em http://localhost:3000'))
+
+server.listen(3000, console.log('server rodando em http://localhost:3000'))
